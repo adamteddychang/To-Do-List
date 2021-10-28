1bc31fad49297bfbfa291ff10462989cc123b1a5
adamteddychang <adamchang0725@gmail.com>
2021-10-28 10:52:58 +0200

    Created module script.js

---
+++ src/script.js
@@ -0,0 +1,25 @@
+const taskCompleted = (e, itemsArr) => {
+  const completed = e.target;
+
+  if (completed.checked) {
+    completed.parentElement.classList.add('completed');
+    for (let i = 0; i < itemsArr.length; i += 1) {
+      const parent = completed.parentElement.id;
+      const itemIndex = itemsArr[i].index;
+      if (parent.toString() === itemIndex.toString()) {
+        itemsArr[i].complete = true;
+        localStorage.setItem('todolist', JSON.stringify(itemsArr));
+      }
+    }
+  } else {
+    completed.parentElement.classList.remove('completed');
+    for (let i = 0; i < itemsArr.length; i += 1) {
+      const parent = completed.parentElement.id;
+      const itemIndex = itemsArr[i].index;
+      if (parent.toString() === itemIndex.toString()) {
+        itemsArr[i].complete = false;
+        localStorage.setItem('todolist', JSON.stringify(itemsArr));
+      }
+    }
+  }
+};
